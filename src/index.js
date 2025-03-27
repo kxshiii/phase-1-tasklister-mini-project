8ad0f59ce4c28456310756160c9e43acf4188a42
kxshiii <brian.gikaru@student.moringaschool.com>
2025-03-27 11:05:38 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-    const form = document.getElementById("create-task-form");
-    const taskList = document.getElementById("tasks");
-  
-    form.addEventListener("submit", (event) => {
-      event.preventDefault(); // Prevent the default form submission behavior
-  
-      const taskDescription = document.getElementById("new-task-description").value;
-  
-      if (taskDescription.trim() !== "") {
-        const taskItem = document.createElement("li");
-        taskItem.textContent = taskDescription;
-  
-        taskList.appendChild(taskItem);
-        form.reset(); // Clear the input field after adding the task
-      }
-    });
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    const taskDescription = document.getElementById("new-task-description").value;
+
+    if (taskDescription.trim() !== "") {
+      const taskItem = document.createElement("li");
+      taskItem.textContent = taskDescription;
+
+      taskList.appendChild(taskItem);
+      form.reset(); // Clear the input field after adding the task
+    }
   });
+});
